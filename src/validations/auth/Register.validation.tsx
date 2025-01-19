87fc944ed9ce8928adbd7f52a35e18bef25c09ff
kwizera-bonheur123 <kbonheur123@gmail.com>
2025-01-19 13:36:26 +0200

    fix validations

--- src/validations/auth/Register.validation.tsx
+++ src/validations/auth/Register.validation.tsx
@@ -1,20 +1,14 @@
 import { z } from "zod";
 
-export const RegisterSchema = z
-  .object({
-    firstName: z.string().min(1, { message: "First name required" }),
-    lastName: z.string().min(1, { message: "Last name required" }),
-    email: z.string().email({ message: "Invalid email address" }),
-    password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, {
-      message:
-        "Password must be at least 8 characters long and include a lowercase letter, an uppercase letter, and a digit.",
-    }),
-    confirmPassword: z.string(),
-  })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
-  })
-  .transform(({ confirmPassword, ...rest }) => rest);
+export const RegisterSchema = z.object({
+  firstName: z.string().min(1, { message: "First name required" }),
+  lastName: z.string().min(1, { message: "Last name required" }),
+  email: z.string().email({ message: "Invalid email address" }),
+  password: z.string().regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/, {
+    message:
+      "Password must be at least 8 characters long and include a lowercase letter, an uppercase letter, and a digit.",
+  }),
+  confirmPassword: z.string(),
+});
 
 export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
